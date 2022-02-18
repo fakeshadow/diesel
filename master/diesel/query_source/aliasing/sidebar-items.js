a89337921a24eabd78f39be908e962fd812e835c
weiznich <github@weiznich.de>
2022-02-18 17:17:25 +0000

    Deploying to gh-pages from  @ 95d1eabf10a5f4318bf7e2490bc626e02facefd1 🚀

---
+++ master/diesel/query_source/aliasing/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Alias","Represents an alias within diesel’s query builder"],["AliasedField","Represents an aliased field (column) within diesel’s query builder"]],"trait":[["AliasSource","Types created by the `alias!` macro that serve to distinguish between aliases implement this trait."],["FieldAliasMapper","Serves to map `Self` to `Alias<S>`"]]});
